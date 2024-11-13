ca24f831f22d89c435a8e9acff7cd66c59e930d6
Etienne Verschave <etienneverschave@gmail.com>
2024-11-13 16:13:39 +0100

    Rails Yelp MVP finish

---
+++ app/models/review.rb
@@ -0,0 +1,5 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :rating, :inclusion => { :in => 0..5, :message => " should be between 0 to 5" }, numericality: { only_integer: true }
+  validates :rating, :content, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+# 2. Create the instances 🏗️
+puts "Creating restaurants..."
+Restaurant.create!(name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "french")
+puts "Created Dishoom"
+
+Restaurant.create!(name: "Le Petit Cambodge", address: "20 Rue Alibert, 75010 Paris, France", category: "french")
+puts "Created Le Petit Cambodge"
+
+Restaurant.create!(name: "Burgermeister", address: "Oberbaumstraße 8, 10997 Berlin, Germany", category: "belgian")
+puts "Created Burgermeister"
+
+Restaurant.create!(name: "Pizza East", address: "56 Shoreditch High St, London E1 6JJ, UK", category: "italian")
+puts "Created Pizza East"
+
+Restaurant.create!(name: "Ippudo NY", address: "65 4th Ave, New York, NY 10003, USA", category: "japanese")
+puts "Created Ippudo NY"
+
+
+# 3. Display a message 🎉
+puts "Finished! Created #{Restaurant.count} restaurants."
